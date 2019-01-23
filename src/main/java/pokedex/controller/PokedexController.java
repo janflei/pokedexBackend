ecdf14df6afe0275988b9628588284fc7d7efe60
janflei <fleisch.ja@gmail.com>
2019-01-23 12:22:29 +0100

    new controller/endpoint/model generation

---
+++ src/main/java/pokedex/controller/PokedexController.java
@@ -0,0 +1,70 @@
+package pokedex.controller;
+
+// Start of user code (user defined imports)
+
+// End of user code
+
+public class PokedexController {
+	/**
+	 * Description of the property history.
+	 */
+	public model.History history = null;
+	
+	/**
+	 * Description of the property restcontroller.
+	 */
+	public controller.RestController restcontroller = null;
+	
+	/**
+	 * Description of the property apicontroller.
+	 */
+	public endpoint.APIController apicontroller = null;
+	
+	// Start of user code (user defined attributes)
+	
+	// End of user code
+	
+
+	private static PokedexController INSTANCE;
+	
+	private PokedexController(){
+	    // singleton
+	}
+	
+	public static PokedexController getInstance(){
+	    if(INSTANCE == null){
+	        INSTANCE = new PokedexController();
+	    }
+	
+	    return INSTANCE;
+	}
+	
+	public void request(String pokeName) throws Exception {
+		// Start of user code request
+		
+		// End of user code
+	}
+	
+	public void getHistory(model.History pokeHistory) throws Exception {
+		// Start of user code getHistory
+		
+		// End of user code
+	}
+	
+	private void addToHistory(model.Pokemon pokemon) throws Exception {
+		// Start of user code addToHistory
+		
+		// End of user code
+	}
+	
+	private void respond(model.Pokemon pokemon) throws Exception {
+		// Start of user code respond
+		
+		// End of user code
+	}
+	
+	// Start of user code (user defined operations)
+	
+	// End of user code
+	
+}
---
+++ src/main/java/pokedex/endpoint/APIController.java
@@ -0,0 +1,17 @@
+package pokedex.endpoint;
+
+// Start of user code (user defined imports)
+
+// End of user code
+
+@org.springframework.web.bind.annotation.RestController
+public class APIController {
+	// Start of user code (user defined attributes)
+	
+	// End of user code
+	
+	
+	// Start of user code (user defined operations)
+	
+	// End of user code
+}
