80384e7c0c3918cfca0266364e24ef7d84ac7a4b
janflei <fleisch.ja@gmail.com>
2019-02-02 18:35:29 +0100

    new static page

---
+++ src/main/resources/static/precache-manifest.d02f46ccf56be0713f331abcf8b90a91.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "b489d2245be1b3fc6936",
+    "url": "/static/js/main.b489d224.chunk.js"
+  },
+  {
+    "revision": "698caf60066d597eaf7c",
+    "url": "/static/js/1.698caf60.chunk.js"
+  },
+  {
+    "revision": "b489d2245be1b3fc6936",
+    "url": "/static/css/main.ca96d9dd.chunk.css"
+  },
+  {
+    "revision": "b7f466233a655660e2d95d30c5461943",
+    "url": "/index.html"
+  }
+];
